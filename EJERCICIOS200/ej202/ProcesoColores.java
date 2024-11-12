58e13c4ab1b22e14f7d606c1dcdb374898ed8757
facucastellano19 <castellanofacundo05@gmail.com>
2024-11-12 17:04:59 -0300

    Update ProcesoColores.java

--- EJERCICIOS200/ej202/ProcesoColores.java
+++ EJERCICIOS200/ej202/ProcesoColores.java
@@ -5,6 +5,13 @@ import java.io.FileNotFoundException;
 import java.util.ArrayList;
 import java.util.Scanner;
 
+//202- Escribir un programa que deje ingresar por consola valores en palabras que representan colores.
+//Estos valores ingresados se deberán verificar si son parte de colores válidos para nuestro programa. 
+//La validación se hará buscando el valor ingresado en una lista de colores, y para armar esta lista, se 
+//deberá leer el contenido de un archivo de texto. En ese archivo, cada valor de color debe ser una linea 
+//de texto del archivo. Terminar el proceso ingresando asterisco.   
+//Negro azul marrón gris verde naranja rosa púrpura rojo blanco amarillo turquesa verde oliva verde menta borgoña lavandamagenta salmón cian beige rosado verde oscuro verde oliva lila amarillo pálido fucsia mostaza, ETC
+    
 public class ProcesoColores {
 
     private ArrayList<String> coloresValidos;
