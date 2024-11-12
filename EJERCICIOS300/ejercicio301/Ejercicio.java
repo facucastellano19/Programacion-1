69007fdcce90ba1d4a236db764178593fb58c097
facucastellano19 <castellanofacundo05@gmail.com>
2024-11-12 16:55:07 -0300

    Update Ejercicio.java

--- EJERCICIOS300/ejercicio301/Ejercicio.java
+++ EJERCICIOS300/ejercicio301/Ejercicio.java
@@ -1,8 +1,15 @@
-// Luca Ferrante
 package ejercicio301;
 
 import java.util.Scanner;
 
+//301 – Realizar un programa en lenguaje java para el cálculo del factorial de un numero. Hacer dos 
+//tipos de a implementaciones, una usando el método iterativo, y otro utilizando recursividad. Debido a 
+//que el resultado de un factorial crece rápidamente, implementar un control en el ingreso, para que no 
+//se pueda llegar a calcular un número ingresado mayor de un límite. Ese límite debe averiguarse 
+//previamente, y colocarlo como un parámetro interno de la clase que hace el cálculo. Ese límite debe 
+//servir para que el cálculo del factorial no termine siendo demasiado grande para ser representado 
+//correctamente por un tipo de dato long.
+    
 public class Ejercicio {
     public void menu() {
 
