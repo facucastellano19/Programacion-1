1d2d144cee4c0d7e7a0b51d799b50868738cdd84
facucastellano19 <castellanofacundo05@gmail.com>
2024-11-12 17:05:39 -0300

    Update PalabrasPila.java

--- EJERCICIOS200/ej204/PalabrasPila.java
+++ EJERCICIOS200/ej204/PalabrasPila.java
@@ -4,6 +4,13 @@ import java.util.ArrayList;
 import java.util.Scanner;
 import java.util.Stack;
 
+//204 - Realizar un programa que deje ingresar palabras desde consola, y la lista de ingresos debe 
+//guardarse en un objeto ArrayList, el fin del ingreso se hace con un asterisco. Luego implementar un 
+//método que haga lo siguiente:
+//a. recorrer el ArrayList y colocar todos los elementos en un objeto Stack (pila).
+//b. recorrer el objeto Stack, y sacar de la pila uno a uno los elementos, e imprimirlos en pantalla.
+//c. Responder, cual es el orden de la impresión de salida respecto a la entrada ?
+
 public class PalabrasPila {
 
     private ArrayList<String> palabras = new ArrayList<>();
