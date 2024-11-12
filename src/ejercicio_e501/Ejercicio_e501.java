1431ac8695242ed44851aceaea8d82870ac76ae7
facucastellano19 <castellanofacundo05@gmail.com>
2024-11-12 16:16:05 -0300

    Add files via upload

---
+++ src/ejercicio_e501/Ejercicio_e501.java
@@ -0,0 +1,14 @@
+
+package ejercicio_e501;
+
+
+public class Ejercicio_e501 {
+
+    public static void main(String[] args) {
+        
+        Ejercicio ej = new Ejercicio();
+        ej.menu();
+        
+    }
+    
+}
